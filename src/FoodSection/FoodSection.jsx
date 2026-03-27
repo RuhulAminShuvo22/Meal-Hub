f9a1f3fd4e9c8e82debc02e9cd3cf12718601171
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-28 02:07:20 +0600

    different cart section added for the added btn for add to cart

--- src/FoodSection/FoodSection.jsx
+++ src/FoodSection/FoodSection.jsx
@@ -1,17 +1,40 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import SingleFood from '../components/SingleFood/SingleFood';
+import Cart from '../components/Cart/Cart';
 
 const FoodSection = ({foodDataRes}) => {
 
     
     const data = use(foodDataRes);
     const foods = data.meals
-    console.log(foods)
+    
+    const [cart, setCart] = useState([])
+
+    const addToCart = (foodData) => {
+        console.log(foodData)
+    }
+    
+
+
     return (
-        <div className='food-container'>
+        <div className='food-container-parent'>
+
+            <div style={{width:"75%"}} className='food-container'>
             {
-                foods.map(food=> <SingleFood food= {food}></SingleFood>)
+                foods.map(food=> <SingleFood addToCart={addToCart} food= {food}></SingleFood>)
             }
+        </div>
+        {/* .................................................................... */}
+        
+        
+        <div style={{backgroundColor:"cyan", width:"75%"}}>
+            <h1>card</h1>
+            <Cart cart={cart}> </Cart>
+
+        </div>
+        {/* ............................ */}
+
+
         </div>
     );
 };
--- src/components/SingleFood/SingleFood.jsx
+++ src/components/SingleFood/SingleFood.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./SingleFood.css"
 
-const SingleFood = ({food}) => {
+const SingleFood = ({food, addToCart}) => {
     console.log(food)
     return (
         <div className='card'>
             <img src={food.strMealThumb} alt="" />
             <h1>{food.strMeal}</h1>
-            <button className='btn-card'>Add to Card</button>
+            <button onClick={()=>addToCart(food)} className='btn-card'>Add to Cart</button>
         </div>
     );
 };
